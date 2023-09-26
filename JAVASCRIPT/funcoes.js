4db3d8dd8f5d42eed8a7dbc2ec27676cb6a3105e
Gabriel Biankati <128652887+gabbiankati@users.noreply.github.com>
2023-09-26 20:50:40 -0300

    Add files via upload

---
+++ JAVASCRIPT/funcoes.js
@@ -0,0 +1,12 @@
+function dizerOla(nome){
+    return (`Olá, ${nome}!`);
+}
+
+function somar(x ,y) {
+    return x + y;
+}
+
+module.exports = {
+    dizerOla, somar
+}
+
---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,6 @@
+const { dizerOla, somar } = require('./funcoes')
+
+const mensagem = dizerOla('Junkes Tur');
+console.log(mensagem)
+console.log(somar(1, 3));
+
